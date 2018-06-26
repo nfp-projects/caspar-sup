e803cf0df0c18d0412222e33bb4a83c47b2a525a
Jonatan Nilsson <jonatan@nilsson.is>
2018-06-26 19:19:38 +0000

    more windows changes

--- install_run.bat
+++
@@ -1,7 +0,0 @@
-git pull
-npm install
-npm run prod-run | bunyan
-echo.
-echo EXITED
-echo.
-PAUSE
---
+++ update_run.bat
@@ -0,0 +1,8 @@
+git pull master
+CALL npm install
+CALL npm run build
+CALL npm run start:win
+echo.
+echo EXITED
+echo.
+PAUSE
